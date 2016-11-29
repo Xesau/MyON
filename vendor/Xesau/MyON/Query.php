937f762a0f1833f64d90ec10c63ffdd540ff2b33
Xesau <aron2000@zeelandnet.nl>
2016-11-30 00:12:58 +0100

    Fixes

--- vendor/Xesau/MyON/Query.php
+++ vendor/Xesau/MyON/Query.php
@@ -2,108 +2,127 @@
 
 namespace Xesau\MyON;
 
-abstract class Query {
-	
-	protected $mainWhereGroup = null;
-	protected $orderRules = [];
-	
-	protected $offset = false;
-	protected $limit = false;
-	
-    /** 
-     * Limits the selection to rows where the given field follows the given condition
-     * 
+use InvalidArgumentException;
+
+abstract class Query
+{
+    protected $mainWhereGroup = null;
+    protected $orderRules = [];
+
+    protected $offset = false;
+    protected $limit = false;
+
+    /**
+     * Limits the selection to rows where the given field follows the given condition.
+     *
      * @param string|string[] $field    The field that must follow the condition
      * @param string          $operator The operator (=, !=, etc.)
      * @param mixed|Selection $value    The value accompanying the operator
      * @param bool            $continue Whether to continue with the newly created group
+     *
      * @return $this|WhereGroup See param $continue
      */
-	public function where($field, $operator, $value, $continue = false) {
-		if ($this->mainWhereGroup == null) {
-			$this->mainWhereGroup = ($newGroup = new WhereGroup(new Where($field, $operator, $value), $this));
-		} else {
-			$newGroup = (new WhereGroup($this->mainWhereGroup, $this))->andWhere($field, $operator, $value);
-            $this->mainWhereGroup = $newGroup;
-		}
-        
-        return $continue ? $newGroup : $this;
-	}
-    
-    /** 
-     * Limits the selection to rows where the given field follows the given condition or the previous condition
-     * 
-     * @param string|string[] $field    The field that must follow the condition
-     * @param string          $operator The operator (=, !=, etc.)
-     * @param mixed|Selection $value    The value accompanying the operator
-     * @param bool            $continue Whether to continue with the newly created group
-     * @return $this|WhereGroup See param $continue
-     */
-    public function orWhere($field, $operator, $value, $continue = false) {
+    public function where($field, $operator, $value, $continue = false)
+    {
         if ($this->mainWhereGroup == null) {
-			$this->mainWhereGroup = ($newGroup = new WhereGroup(new Where($field, $operator, $value), $this));
-		} else{
-			$newGroup = (new WhereGroup($this->mainWhereGroup, $this))->orWhere($field, $operator, $value);
+            $this->mainWhereGroup = ($newGroup = new WhereGroup(new Where($field, $operator, $value), $this));
+        } else {
+            $newGroup = (new WhereGroup($this->mainWhereGroup, $this))->andWhere($field, $operator, $value);
             $this->mainWhereGroup = $newGroup;
-		}
-        
+        }
+
         return $continue ? $newGroup : $this;
     }
-	
-	public function asc($field) {
-		$this->orderRules[] = new Order($field, 'asc');
+
+    /**
+     * Limits the selection to rows where the given field follows the given condition or the previous condition.
+     *
+     * @param string|string[] $field    The field that must follow the condition
+     * @param string          $operator The operator (=, !=, etc.)
+     * @param mixed|Selection $value    The value accompanying the operator
+     * @param bool            $continue Whether to continue with the newly created group
+     *
+     * @return $this|WhereGroup See param $continue
+     */
+    public function orWhere($field, $operator, $value, $continue = false)
+    {
+        if ($this->mainWhereGroup == null) {
+            $this->mainWhereGroup = ($newGroup = new WhereGroup(new Where($field, $operator, $value), $this));
+        } else {
+            $newGroup = (new WhereGroup($this->mainWhereGroup, $this))->orWhere($field, $operator, $value);
+            $this->mainWhereGroup = $newGroup;
+        }
+
+        return $continue ? $newGroup : $this;
+    }
+
+    public function asc($field)
+    {
+        $this->orderRules[] = new Order($field, 'asc');
+
         return $this;
-	}
-	
-	public function desc($field) {
-		$this->orderRules[] = new Order($field, 'desc');
+    }
+
+    public function desc($field)
+    {
+        $this->orderRules[] = new Order($field, 'desc');
+
         return $this;
-	}
-	
-	public function orderField($field, array $values) {
-		$this->orderRules[] = new OrderByField($field, $values);
+    }
+
+    public function orderField($field, array $values)
+    {
+        $this->orderRules[] = new OrderByField($field, $values);
+
         return $this;
-	}
-    
+    }
+
     /**
      * Gets or sets the amount of rows offset in the result of this query.
-     * 
+     *
      * @param int|bool|null $offset The amount of rows to be offset. To disable the offset, specify FALSE.
      *                              To get the amount of rows to be offset, specify NULL.
+     *
      * @return $this|int|bool If $offset is not specified, the currently set amount is returned, or FALSE
      *                        is returned if no amount has been set. If $offset is specified, the
      *                        current amount will be updated and the current object will be returned.
      */
-    public function offset($offset = null) {
-        if ($offset === null)
+    public function offset($offset = null)
+    {
+        if ($offset === null) {
             return $this->offset;
-        
+        }
+
         if ($offset !== false) {
             if (is_int($offset)) {
-                if ($offset < 1) {
-                    throw new InvalidArgumentException('Query.limit $offset must be an integer greater than 0, or -1 to disable the limit.');
+                if ($offset < 0) {
+                    throw new InvalidArgumentException('Query.offset $offset must be an integer equal to or greater than 0, or -1 to disable the offset.');
                 }
             } else {
-                throw new InvalidArgumentException('Query.limit $offset is not an int or FALSE.');
+                throw new InvalidArgumentException('Query.offset $offset is not an int or FALSE.');
             }
         }
-        
+
         $this->offset = $offset;
+
         return $this;
     }
 
     /**
      * Gets or sets the limit of the result of this query.
-     * 
+     *
      * @param int|bool|null $limit The limit to set. To disable the limit, specify FALSE. To get the limit, specify NULL.
+     *
      * @return $this|int|bool If $limit is not specified, the currently set limit is returned, or FALSE
-     *                       is returned if a limit has not been set. If $limit is specified, the
-     *                       current limit will be updated and the current object will be returned.
+     *                        is returned if a limit has not been set. If $limit is specified, the
+     *                        current limit will be updated and the current object will be returned.
      */
-    public function limit($limit = null) {
-        if ($limit === null)
+    public function limit($limit = null)
+    {
+        if ($limit === null) {
             return $this->limit;
-        
+        }
+
         if ($limit !== false) {
             if (is_int($limit)) {
                 if ($limit < 1) {
@@ -113,9 +132,9 @@ abstract class Query {
                 throw new InvalidArgumentException('Query.limit $limit is not an int or FALSE.');
             }
         }
-        
+
         $this->limit = $limit;
+
         return $this;
     }
-    
 }
